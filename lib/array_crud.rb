a9431f8636000a2da8b795bfd27d9034e29e65f6
Joseph Choi <jdc07@live.com>
2020-02-15 12:01:03 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,15 +1,17 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  numbers = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
+  numbers = [1,2,3,4]
   
+  numbers << "5"
 end
-
+  
 def add_element_to_start_of_array(array, element)
   
 end
